51474b29ab17c3568f4dbea8d52e3be56eee16dc
Jason Bollinger <jrb6996@psu.edu>
2025-01-28 11:13:23 -0500

    cleaning up jrb's mess

---
+++ lab03/lab03_dnd_agent.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parents[1]))
+
+from ollama import chat
+from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
+
+# Add you code below
+sign_your_name = 'Jason Bollinger'
+model = 'llama3.2'
+messages = [
+  {'role': 'system', 'content': 'You should have emotions like a human being \
+                                 and be able to convey those emotions in your responses.'},
+]
+options = {'temperature': 0.5, 'max_tokens': 100}
+
+
+# But before here.
+
+options |= {'seed': seed(sign_your_name)}
+# Chat loop
+while True:
+  response = chat(model=model, messages=messages, stream=False, options=options)
+  # Add your code below
+  
+  message = {'role': 'user', 'content': input('You: ')}
+  messages.append(message)
+  response = chat(model=model, messages=messages, stream=False, options=options)
+  print(f'Agent: {response.message.content}')
+  messages.append({'role': 'assistant', 'content': response.message.content})
+
+  # But before here.
+  if messages[-1]['content'] == '/exit':
+    break
+
+# Save chat
+with open(Path('lab03/attempts.txt'), 'a') as f:
+  file_string  = ''
+  file_string +=       '-------------------------NEW ATTEMPT-------------------------\n\n\n'
+  file_string += f'Model: {model}\n'
+  file_string += f'Options: {options}\n'
+  file_string += pretty_stringify_chat(messages)
+  file_string += '\n\n\n------------------------END OF ATTEMPT------------------------\n\n\n'
+  f.write(file_string)
+
